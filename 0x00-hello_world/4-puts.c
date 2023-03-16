308a27ee259ce5d2b1331baf170cd6b371652a5d
yassminefanid <yassmine123fanid@gmail.com>
2023-03-16 12:55:20 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #!/bin/bash
 #include <stdio.h>
-int main(void)
-{
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+
+int main() {
+    char *message = "Programming is like building a multilingual puzzle";
+    puts(message);
+    return 0;
 }
