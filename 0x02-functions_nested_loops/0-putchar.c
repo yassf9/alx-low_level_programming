3c73e043d6aae2b97cbc2dc3524aeb27c747b094
yassminefanid <yassmine123fanid@gmail.com>
2023-03-21 12:32:41 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <unistd.h>
 /**
- * _putchar - writres the character c to stdout
+ * _putchar - writes the character c to stdout
  * @c: the character to print
  *
  * Return: On success 1.
