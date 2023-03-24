ab998362e00a133defa09d025c8f66f672eca5a3
yassminefanid <yassmine123fanid@gmail.com>
2023-03-24 16:03:20 -0700

    fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,14 +14,14 @@ int main(void)
 int i;
 for (i = 1; i <= 100; i++)
 {
-if (i % 3 == 0)
-printf("Fizz");
+if (i % 5 == 0 && i % 3 == 0)
+printf("FizzBuzz ");
+else if (i % 3 == 0)
+printf("Fizz ");
 else if (i % 5 == 0)
-printf("Buzz");
-else if ((i % 5 == 0) && (i % 3 == 0))
-printf("FizzBuzz");
+printf("Buzz ");
 else
-printf("%d\t", i);
+printf("%d ", i);
 }
 return (0);
 }
