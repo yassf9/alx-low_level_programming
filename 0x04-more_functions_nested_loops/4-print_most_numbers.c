65aec9e41aa83a11142c69a34c8be9fc3b366d3b
yassminefanid <yassmine123fanid@gmail.com>
2023-03-24 12:42:24 -0700

    mprint

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_most_numbers - prints the numbers 01356789 ,followed by a new line
+ * Return: Always 0
+ */
+void print_most_numbers(void)
+{
+char c;
+for (c = '0'; c <= '9'; c++)
+{
+if (!(c == '2' || c == '9'))
+{
+_putchar(c);
+}
+}
+_putchar('\n');
+}
