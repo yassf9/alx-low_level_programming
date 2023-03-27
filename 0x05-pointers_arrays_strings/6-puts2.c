bf5c49485794a2df4b507af96c49aafb2acc3b0f
yassminefanid <yassmine123fanid@gmail.com>
2023-03-27 12:25:54 -0700

    put

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * puts2 - function should print only one character out of two
+ * starting with the first one
+ * @str: input
+ * Return: print
+ */
+void puts2(char *str)
+{
+	int longi = 0;
+	char *y = str;
+	int o;
+
+	while (*y != '\0')
+	{
+		y++;
+		longi++;
+	}
+	for (o = 0 ; o < longi ; o++)
+	{
+		if (o % 2 == 0)
+	{
+		_putchar(str[o]);
+	}
+	}
+	_putchar('\n');
+}
