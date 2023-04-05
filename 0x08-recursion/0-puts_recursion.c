d97f09c65afd2506254ba245d87b7b23962af5df
yassminefanid <yassmine123fanid@gmail.com>
2023-04-05 13:44:59 -0700

    0. She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -4,14 +4,15 @@
 * @s:input
 * Return:Always 0
 */
+
 void _puts_recursion(char *s)
 {
 	if (*s == '\0')
 	{
 		_putchar('\n');
-	  return;
+		return;
 	}
-	else
-		_putchar(*s);
+
+	_putchar(*s);
 	_puts_recursion(s + 1);
 }
