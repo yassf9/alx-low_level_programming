e51a3cd11980116368cace96714fda84a3f942b9
yassminefanid <yassmine123fanid@gmail.com>
2023-04-11 11:39:34 -0700

    1. The woman who has no imagination has no wings

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * *_strdup - a pointer to a newly allocated space in memory
+ * @str: string
+ * Return: pointer to the duplicate,NULL if str = NULL
+ */
+char *_strdup(char *str)
+{
+	 char *string;
+	int i, len = 0;
+
+	if (str == NULL)
+		return (NULL);
+	for (i = 0; str[i] > '\0'; i++)
+		len++;
+	string = (char *)malloc(sizeof(char) * len);
+
+	if (string == NULL)
+		return (NULL);
+	for (i = 0; i < len; i++)
+		string[i] = str[i];
+	string[len] = '\0';
+	return (string);
+}
