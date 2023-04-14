dd020ec715050c9fe74c33289d187071ac01792b
yassminefanid <yassmine123fanid@gmail.com>
2023-04-13 17:05:01 -0700

    0. Trust no one

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * malloc_checked -  allocates memory
+ * @b: unsigned int
+ * Return: pointer (Success), 98 (fails)
+ */
+void *malloc_checked(unsigned int b)
+{
+void *ptr = malloc(b);
+
+if (ptr == NULL)
+{
+exit(98);
+}
+return (ptr);
+}
