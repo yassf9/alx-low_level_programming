efc76353500f12b7f9d86ff862e2dea962b965e6
yassminefanid <yassmine123fanid@gmail.com>
2023-04-17 15:36:07 -0700

    4. A door is what a dog is perpetually on the wrong side of

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,6 +2,45 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: length
+ */
+int _strlen(char *s)
+{
+	int longi = 0;
+
+	while (*s != '\0')
+	{
+		longi++;
+		s++;
+	}
+
+	return (longi);
+}
+/**
+ * char *_strcpy - a function that copies the string pointed to by src
+ * @dest: copy to
+ * @src: copy from
+ * Return: string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int l = 0;
+	int x = 0;
+
+	while (*(src + l) != '\0')
+	{
+		l++;
+	}
+	for ( ; x < l ; x++)
+	{
+		dest[x] = src[x];
+	}
+	dest[l] = '\0';
+	return (dest);
+}
+/**
  * new_dog -  function that creates a new dog
  * @name: pointer to string name
  * @age: age number
@@ -15,27 +54,21 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	if (!name || age < 0 || !owner)
 		return (NULL);
-	while (name[len1] != '\0')
-		len1++;
-	while (owner[len2] != '\0')
-		len2++;
 	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
 	{
 		free(dog);
 		return (NULL);
 	}
-	dog->name = malloc(len1 * sizeof(dog->name));
+	dog->name = malloc(_strlen(name) * sizeof(dog->name));
 	if (dog->name == NULL)
 	{
 		free(dog->name);
 		free(dog);
 		return (NULL);
 	}
-	for (i = 0; i < len1; i++)
-		dog->name[i] = name[i];
 	dog->age = age;
-	dog->owner = malloc(len2 * sizeof(dog->owner));
+	dog->owner = malloc(_strlen(owner) * sizeof(dog->owner));
 	if (dog->owner == NULL)
 	{
 		free(dog->owner);
@@ -43,7 +76,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(dog);
 		return (NULL);
 	}
-	for (i = 0; i < len2; i++)
-		dog->owner[i] = owner[i];
+	dog->name = _strcpy(dog->name, name);
+	dog->owner = _strcpy(dog->owner, owner);
 	return (dog);
 }
