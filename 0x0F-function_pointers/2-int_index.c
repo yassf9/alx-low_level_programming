e5c79a3f4fc6a2544892ad00c3aa72913d93c7ad
yassminefanid <yassmine123fanid@gmail.com>
2023-04-19 15:36:03 -0700

    o hell with circumstances; I create opportunities

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,25 @@
+#include "function_pointers.h"
+#include <stddef.h>
+/**
+ * int_index - earches for an integer
+ * @array: array of integers
+ * @size: size of array
+ * @cmp: pointer to the function to be used to compare values
+ * Return: index(Success) ,-1 (fail)
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array != NULL && size > 0 && cmp != NULL)
+	{
+		if (size <= 0)
+			return (-1);
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]) != 0)
+				return (i);
+		}
+	}
+	return (-1);
+}
