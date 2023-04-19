d2741836b2ca51f05dabe764927cc572edca839e
yassminefanid <yassmine123fanid@gmail.com>
2023-04-19 14:20:35 -0700

    0. What's my name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_name - function that prints a name
+ * @name: pointer to a string name
+ * @(*f)(char *): points to a function that takes a char pointer
+ * Return: nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
