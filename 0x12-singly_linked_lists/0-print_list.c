a8d3d13a59a687adfac8771aa560cbaff2c31a42
Yassmine <yassmine123fanid@gmail.com>
2023-04-27 10:59:00 -0700

    0. Print list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * print_list - prints all the elements of a list_t list
+ * @h: pointer to list_t
+ * Return: he number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	int cmp = 0, i;
+
+	if (str == NULL)
+		printf("[0] (nil)");
+	else
+	{
+		for (i = 0; i < (int)h->len; i++)
+			cmp = ++;
+		return (cmp);
+	}
+}
