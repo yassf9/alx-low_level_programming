05d20e75478d1b92456be82c60c548f6288160f1
Yassmine <yassmine123fanid@gmail.com>
2023-04-30 03:11:10 -0700

    advanced

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * before_main - runs before main 
+ * Return: nothing
+ */
+void __attribute__ ((constructor)) before_main()
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
