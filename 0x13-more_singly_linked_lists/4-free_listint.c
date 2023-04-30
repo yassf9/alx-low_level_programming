4922da37637a724fedc27cf8919f0bd04c837c6d
Yassmine <yassmine123fanid@gmail.com>
2023-04-30 06:24:50 -0700

    task4

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_listint - frees a listint_t list
+ * @head: pointer to listint_t
+ *Return: nothing
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *tmp;
+
+	while (head != NULL)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp);
+	}
+}
