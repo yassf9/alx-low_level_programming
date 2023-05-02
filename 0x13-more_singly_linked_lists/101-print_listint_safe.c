a70fe919e40fdc8c6ee94742740edb3b7399b9f7
Yassmine <yassmine123fanid@gmail.com>
2023-05-02 04:16:53 -0700

    fix

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -16,24 +16,25 @@ size_t print_listint_safe(const listint_t *head)
 		exit(98);
 	while (hare != NULL && hare->next != NULL)
 	{
-		printf("[%p] %d\n", (void *)tortoise, tortoise->n);
 		tortoise = tortoise->next;
 		hare = hare->next->next;
+		printf("[%p] %d\n", (void *)tortoise, tortoise->n);
+		count++;
 		/* check if hare has caught up to tortoise */
 		if (hare == tortoise)
 		{
-			/* print the value of the node where the loop starts */
-			printf("[%p] %d\n", (void *)hare, hare->n);
-			exit(98);
+			tortoise = head;
+			while ( tortoise != hare)
+			{
+				/* print the value of the node where the loop starts */
+				printf("[%p] %d\n", (void *)tortoise, tortoise->n);
+				tortoise = tortoise->next;
+				hare = hare->next;
+				count++;
+			}
+			 printf("->[%p] %d\n", (void *)tortoise, tortoise->n);
+			 exit(98);
 		}
-		count++;
-	}
-
-	/* print the last node in the list */
-	if (hare != NULL)
-	{
-		printf("[%p] %d\n", (void *)tortoise, tortoise->n);
-		count++;
 	}
 	return (count);
 }
